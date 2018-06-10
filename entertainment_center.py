758d9df8ddbfedd3ee2db888d7f4169d20a4b915
Timothy Henderson <madcap0001@gmail.com>
2018-06-09 20:16:39 -0500

    Edited to remove excess line length

--- entertainment_center.py
+++ entertainment_center.py
@@ -16,20 +16,22 @@ toyStory = media.Movie("Toy Story",
         "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
         "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 avatar = media.Movie("Avatar",
-        "https://upload.wikimedia.org/wikipedia/sco/b/b0/Avatar-Teaser-Poster.jpg",
-        "https://www.youtube.com/watch?v=5PSNL1qE6VY")
+    "https://upload.wikimedia.org/wikipedia/sco/b/b0/Avatar-Teaser-Poster.jpg",
+    "https://www.youtube.com/watch?v=5PSNL1qE6VY")
 clerks2 = media.Movie("Clerks 2",
-        "https://upload.wikimedia.org/wikipedia/en/0/03/Clerks_II_Theatrical_Poster.jpg",
-        "https://www.youtube.com/watch?v=gLvhJ0m5ask")
+    "https://upload.wikimedia.org/wikipedia/" +
+    "en/0/03/Clerks_II_Theatrical_Poster.jpg",
+    "https://www.youtube.com/watch?v=gLvhJ0m5ask")
 schoolOfRock = media.Movie("School of Rock",
-        "https://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",
-        "https://www.youtube.com/watch?v=3PsUJFEBC74")
+    "https://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",
+    "https://www.youtube.com/watch?v=3PsUJFEBC74")
 hungerGames = media.Movie("Hunger Games",
-        "https://upload.wikimedia.org/wikipedia/en/4/42/HungerGamesPoster.jpg",
-        "https://www.youtube.com/watch?v=RCDHJ6P_y0I")
+    "https://upload.wikimedia.org/wikipedia/en/4/42/HungerGamesPoster.jpg",
+    "https://www.youtube.com/watch?v=RCDHJ6P_y0I")
 infinityWar = media.Movie("Avengers: Infinity War",
-        "https://upload.wikimedia.org/wikipedia/en/4/4d/Avengers_Infinity_War_poster.jpg",
-        "https://www.youtube.com/watch?v=6ZfuNTqbHE8")
+    "https://upload.wikimedia.org/wikipedia/" +
+    "en/4/4d/Avengers_Infinity_War_poster.jpg",
+    "https://www.youtube.com/watch?v=6ZfuNTqbHE8")
 
 
 #populate array with movie objects then call fresh_tomatoes to open web page
